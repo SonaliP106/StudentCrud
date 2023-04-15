2d5d99f944d64c9e2392c85215b39a9e19b68bf1
SonaliP106 <129957861+SonaliP106@users.noreply.github.com>
2023-04-15 13:43:48 +0530

    Add files via upload

---
+++ operation.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="operation.aspx.cs" Inherits="StudentCrud.operation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="nametxt" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="nametxt" ErrorMessage="enter name"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            Stream&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="streamtxt" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="streamtxt" ErrorMessage="enter stream"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            Age&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="agetxt" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="agetxt" ErrorMessage="enter age"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="bView" runat="server" Text="view" OnClick="bView_Click" />
+
+            <br />
+
+            <asp:GridView ID="DataView" runat="server" DataKeyNames="id" OnRowCancelingEdit="DataView_RowCancelingEdit" OnRowEditing="DataView_RowEditing" OnRowUpdating="DataView_RowUpdating" OnRowDeleting="DataView_RowDeleting">
+                <Columns>
+            <asp:CommandField ShowEditButton="True" />
+            <asp:CommandField ShowDeleteButton="True" />
+        </Columns>
+            </asp:GridView>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ operation.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentCrud
+{
+    public partial class operation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bView_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["studentDBcon"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("stuInsert", con);    
+            cmd.CommandType = CommandType.StoredProcedure;   
+
+
+            cmd.Parameters.AddWithValue("@sName", nametxt.Text.ToString());        
+            cmd.Parameters.AddWithValue("@sStream ", streamtxt.Text.ToString());     
+            cmd.Parameters.AddWithValue("@sAge ", agetxt.Text.ToString());         
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Response.Write(" student data insered");
+
+            Show();
+            
+        }
+        public void Show()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["studentDBcon"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("stuShow", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataView.DataSource = ds;
+            DataView.DataBind();
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            
+        }
+
+        protected void DataView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            DataView.EditIndex = e.NewEditIndex;
+            Show();
+
+        }
+
+        protected void DataView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            DataView.EditIndex = -1;
+            Show();
+        }
+
+        protected void DataView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = int.Parse(DataView.DataKeys[e.RowIndex].Value.ToString());
+            string cs = ConfigurationManager.ConnectionStrings["studentDBcon"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("upStu", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@sName",((TextBox)DataView.Rows[e.RowIndex].Cells[3].Controls[0]).Text.ToString());
+            cmd.Parameters.AddWithValue("@sStream",((TextBox)DataView.Rows[e.RowIndex].Cells[4].Controls[0]).Text.ToString());
+            cmd.Parameters.AddWithValue("@sAge",((TextBox)DataView.Rows[e.RowIndex].Cells[5].Controls[0]).Text.ToString());
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            DataView.EditIndex = -1;
+            Show();
+        }
+
+        protected void DataView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = int.Parse(DataView.DataKeys[e.RowIndex].Value.ToString());
+            string cs = ConfigurationManager.ConnectionStrings["studentDBcon"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delStu", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Show();
+        }
+    }
+}
